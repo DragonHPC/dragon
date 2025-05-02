91aebd4b92638e2cad5122a0058901dee3fa6545
Kent Lee <kent.lee@hpe.com>
2025-05-02 22:16:13 +0000

    prepare for update

--- test/minconnection/test_minconnection.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import shim_dragon_paths
-import unittest
-from minconnection.minconnection_obj_tests import MinConnectionTest
-
-if __name__ == "__main__":
-    unittest.main()
