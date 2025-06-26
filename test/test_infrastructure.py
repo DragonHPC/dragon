c0b98805db799321467aa7bc59287228ca2e242d
Kent Lee <kent.lee@hpe.com>
2025-06-26 15:29:22 +0000

    prepare for update

--- test/test_infrastructure.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import shim_dragon_paths
-import unittest
-from infrastructure.env_parameter_tests import LaunchParameterTest
-from infrastructure.newline_stream_wrapper_test import NewlineStreamWrapperTest
-
-
-if __name__ == "__main__":
-    unittest.main()
